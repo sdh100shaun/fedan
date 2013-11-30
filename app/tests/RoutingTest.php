287df17b4c7eb678796eeea4d94aac35526d4c6c
Shaun Hare <shaun@shaunhare.co.uk>
2013-11-30 11:06:55 +0000

    initial upload after setup

---
+++ app/tests/RoutingTest.php
@@ -0,0 +1,24 @@
+<?php
+
+class RoutingTest extends TestCase {
+
+	/**
+	 * A basic functional routing test setup
+	 * @author shaunhare
+	 * @return void
+	 */
+	public function testBaseRoute()
+	{
+		$crawler = $this->client->request('GET', '/');
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
+    public function testUserRoute()
+    {
+        $crawler = $this->client->request('GET', '/users');
+
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+    
+
+}
